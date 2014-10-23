32329e9d62dbfff06e887f2db21b8743fcb15a74
jeffrm <jeffrm@gmail.com>
2014-10-22 23:21:45 -0200

    adicionando classe com egit

---
+++ src/br/com/yaw/eclipse/model/Indice.java
@@ -0,0 +1,15 @@
+package br.com.yaw.eclipse.model;
+
+public class Indice {
+String codindice;
+
+public String getCodindice() {
+	return codindice;
+}
+
+public void setCodindice(String codindice) {
+	this.codindice = codindice;
+}
+
+
+}
